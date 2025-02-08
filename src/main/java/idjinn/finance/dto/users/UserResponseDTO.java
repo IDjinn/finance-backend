15e160a6525fc896235556eb9ab8442d0e509649
IDjinn <42049518+IDjinn@users.noreply.github.com>
2025-02-08 17:52:52 -0300

    chore: add accounts and cc ids in user dto response

--- src/main/java/idjinn/finance/dto/users/UserResponseDTO.java
+++ src/main/java/idjinn/finance/dto/users/UserResponseDTO.java
@@ -1,20 +1,27 @@
 package idjinn.finance.dto.users;
 
+import idjinn.finance.model.Account;
+import idjinn.finance.model.CreditCard;
 import idjinn.finance.model.User;
 import idjinn.finance.util.Strings;
 
+import java.util.List;
 import java.util.UUID;
 
 public record UserResponseDTO(
         UUID Id,
         String Name,
-        String Email
+        String Email,
+        List<UUID> Accounts,
+        List<UUID> CreditCards
 ) {
     public static UserResponseDTO fromUser(final User user) {
         return new UserResponseDTO(
                 user.getId(),
                 user.getName(),
-                Strings.maskEmail(user.getEmail())
+                Strings.maskEmail(user.getEmail()),
+                user.getAccounts().stream().map(Account::getUuid).toList(),
+                user.getCreditCards().stream().map(CreditCard::getUuid).toList()
         );
     }
 }
