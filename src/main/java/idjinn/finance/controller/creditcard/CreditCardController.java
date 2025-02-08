b8a61cd12796fc3a7fdb595a6afba96cc6d74e31
IDjinn <42049518+IDjinn@users.noreply.github.com>
2025-02-08 17:54:40 -0300

    chore: add docs to credit card controller

--- src/main/java/idjinn/finance/controller/creditcard/CreditCardController.java
+++ src/main/java/idjinn/finance/controller/creditcard/CreditCardController.java
@@ -4,6 +4,10 @@ import idjinn.finance.dto.creditcard.CreateCreditCardDTO;
 import idjinn.finance.dto.creditcard.CreditCardResponseDTO;
 import idjinn.finance.service.CreditCardsService;
 import idjinn.finance.util.errors.common.NotFoundException;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,11 +23,19 @@ public class CreditCardController {
 
     private final CreditCardsService creditCardsService;
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Credit Card found", content = @Content(schema = @Schema(implementation = CreditCardResponseDTO.class))),
+            @ApiResponse(responseCode = "404", description = "Credit Card not found", content = @Content)
+    })
     @GetMapping("/{id}")
     public CreditCardResponseDTO getCreditCard(@PathVariable("id") @Valid final UUID id) {
         return creditCardsService.getCreditCard(id).map(CreditCardResponseDTO::fromCreditCard).orElseThrow(() -> new NotFoundException("Credit Card not found"));
     }
 
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Credit Card created", content = @Content(schema = @Schema(implementation = CreditCardResponseDTO.class))),
+            @ApiResponse(responseCode = "409", description = "Credit Card already exists", content = @Content)
+    })
     @PostMapping("/create")
     public ResponseEntity<CreditCardResponseDTO> create(@RequestBody @Valid final CreateCreditCardDTO creditCardDTO) {
         return new ResponseEntity<>(creditCardsService.createCreditCard(creditCardDTO), HttpStatus.CREATED);
